4bfbdcbbbf5a46935a23104f44908e11ea510bd9
Clairton Carneiro Luz <clairton.luz@seduc.ce.gov.br>
2020-10-06 07:52:56 -0300

    test coverage formatPhone

--- src/utils/FormatUtils/formatPhone.spec.ts
+++ src/utils/FormatUtils/formatPhone.spec.ts
@@ -18,57 +18,50 @@ describe('formatPhone', () => {
   });
 
   it('should format the phone with 11 digits', () => {
-    const input = '85999332456';
-    expect(formatPhone(input)).toBe('(85)99933-2456');
+    expect(formatPhone('85999332456')).toBe('(85)99933-2456');
   });
 
   it('should format the phone with 10 digits', () => {
-    const input = '8599933245';
-    expect(formatPhone(input)).toBe('(85)9993-3245');
+    expect(formatPhone('8599933245')).toBe('(85)9993-3245');
   });
 
   it('should format the phone with 9 digits', () => {
-    const input = '859993324';
-    expect(formatPhone(input)).toBe('(85)9993-324');
+    expect(formatPhone('859993324')).toBe('(85)9993-324');
   });
 
   it('should format the phone with 8 digits', () => {
-    const input = '85999332';
-    expect(formatPhone(input)).toBe('(85)9993-32');
+    expect(formatPhone('85999332')).toBe('(85)9993-32');
   });
 
   it('should format the phone with 7 digits', () => {
-    const input = '8599933';
-    expect(formatPhone(input)).toBe('(85)9993-3');
+    expect(formatPhone('8599933')).toBe('(85)9993-3');
   });
 
   it('should format the phone with 6 digits', () => {
-    const input = '859993';
-    expect(formatPhone(input)).toBe('(85)9993');
+    expect(formatPhone('859993')).toBe('(85)9993');
   });
 
   it('should format the phone with 5 digits', () => {
-    const input = '85999';
-    expect(formatPhone(input)).toBe('(85)999');
+    expect(formatPhone('85999')).toBe('(85)999');
   });
 
   it('should format the phone with 4 digits', () => {
-    const input = '8599';
-    expect(formatPhone(input)).toBe('(85)99');
+    expect(formatPhone('8599')).toBe('(85)99');
   });
 
   it('should format the phone with 3 digits', () => {
-    const input = '859';
-    expect(formatPhone(input)).toBe('(85)9');
+    expect(formatPhone('859')).toBe('(85)9');
   });
 
   it('should format the phone with 2 digits', () => {
-    const input = '85';
-    expect(formatPhone(input)).toBe('(85');
+    expect(formatPhone('85')).toBe('(85');
   });
 
   it('should format the phone with 1 digits', () => {
-    const input = '8';
-    expect(formatPhone(input)).toBe('(8');
+    expect(formatPhone('8')).toBe('(8');
+  });
+
+  it('should truncate fones with more than 11 digits and format it', () => {
+    expect(formatPhone('859993324562')).toBe('(85)99933-2456');
   });
 });
--- src/utils/FormatUtils/formatPhone.ts
+++ src/utils/FormatUtils/formatPhone.ts
@@ -2,23 +2,23 @@ import removeNonNumeric from '../StringUtils/removeNonNumeric';
 import isBlank from '../StringUtils/isBlank';
 
 export default (phone?: string | null): string => {
-  if (isBlank(phone)) return '';
   phone = removeNonNumeric(phone);
+  if (isBlank(phone)) return '';
   if (phone.length > 11) phone = phone.substring(0, 11);
+
   if (phone.length === 11) {
-    phone = `(${phone.substring(0, 2)})${phone.substring(
+    return `(${phone.substring(0, 2)})${phone.substring(
       2,
       7
     )}-${phone.substring(7)}`;
   } else if (phone.length > 6) {
-    phone = `(${phone.substring(0, 2)})${phone.substring(
+    return `(${phone.substring(0, 2)})${phone.substring(
       2,
       6
     )}-${phone.substring(6)}`;
   } else if (phone.length > 2) {
-    phone = `(${phone.substring(0, 2)})${phone.substring(2)}`;
-  } else if (phone.length > 0) {
-    phone = `(${phone}`;
+    return `(${phone.substring(0, 2)})${phone.substring(2)}`;
   }
-  return phone;
+
+  return `(${phone}`;
 };
