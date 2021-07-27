e380e87957169c18ca72703da1d1bbc3b8042ff9
Clairton Carneiro Luz <clairton.luz@seduc.ce.gov.br>
2021-07-27 10:12:41 -0300

    fix code smell

--- src/utils/StringUtils/isInteger.ts
+++ src/utils/StringUtils/isInteger.ts
@@ -1,6 +1,4 @@
 export default (value?: string | null): boolean => {
   if (!value || value.includes(' ') || value.includes('.')) return false;
   return !isNaN(+value);
-  // if (!value) return false;
-  // return /^\d+$/.test(value);
 };
